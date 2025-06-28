f70843ac28719de344b03a60c77d4a3de8b5ffb4
ki11errabbit <ki11errabbit@posteo.net>
2025-06-28 16:41:15 -0700

    made rust structures public

--- src/dynamic_x86_64.rs
+++ src/dynamic_x86_64.rs
@@ -10,18 +10,18 @@ use std::ffi::CStr;
 #[repr(C)]
 /// Doubly-linked list of dyn-info
 pub struct unw_dyn_info_t {
-    next: *mut unw_dyn_info_t,
-    prev: *mut unw_dyn_info_t,
+    pub next: *mut unw_dyn_info_t,
+    pub prev: *mut unw_dyn_info_t,
     /// First IP covered by this entry
-    start_ip: usize,
+    pub start_ip: usize,
     /// first IP NOT covered by this entry
-    end_ip: usize,
+    pub end_ip: usize,
     /// global-pointer in effect for this entry
-    global_pointer: usize,
+    pub global_pointer: usize,
     /// real type: unw_dyn_info_format_t
-    format: i32,
-    pad: i32,
-    u: unw_dyn_info_union
+    pub format: i32,
+    pub pad: i32,
+    pub u: unw_dyn_info_union
 }
 
 pub union unw_dyn_info_union {
@@ -32,31 +32,31 @@ pub union unw_dyn_info_union {
 
 #[repr(C)]
 pub struct unw_dyn_proc_info_t {
-    name_ptr: *mut CStr,
-    handler: *mut (),
-    flags: u32,
-    pad0: i32,
-    regions: *mut ()
+    pub name_ptr: *mut CStr,
+    pub handler: *mut (),
+    pub flags: u32,
+    pub pad0: i32,
+    pub regions: *mut ()
 }
 #[repr(C)]
 pub struct unw_dyn_table_info_t {
     /// addr. of table name (e.g., library name)
-    name_pointer: *mut CStr,
+    pub name_pointer: *mut CStr,
     /// segment base
-    segbase: usize,
+    pub segbase: usize,
     /// must be a multiple of sizeos(unw_word_t)
-    table_len: usize,
-    table_data: *mut usize
+    pub table_len: usize,
+    pub table_data: *mut usize
 }
 #[repr(C)]
 pub struct unw_dyn_remote_table_info_t {
     /// addr. of table name (e.g., library name)
-    name_ptr: *mut CStr,
+    pub name_ptr: *mut CStr,
     /// segment base
-    segbase: usize,
+    pub segbase: usize,
     /// must be a multiple of sizeof(unw_word_t)!
-    table_len: usize,
-    table_data: usize,
+    pub table_len: usize,
+    pub table_data: usize,
 }
 
 // functions
