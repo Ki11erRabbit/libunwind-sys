c5730e4304f4976d5ff1ce50d1b22c7ac096ae39
ki11errabbit <ki11errabbit@posteo.net>
2025-06-28 16:38:02 -0700

    added back dynamic libunwind Rust structures

--- src/dynamic_x86_64.rs
+++ src/dynamic_x86_64.rs
@@ -3,9 +3,59 @@
 #![allow(non_snake_case)]
 
 
-pub use crate::*;
+use crate::*;
 
+#[repr(C)]
+/// Doubly-linked list of dyn-info
+pub struct unw_dyn_info_t {
+    next: *mut unw_dyn_info_t,
+    prev: *mut unw_dyn_info_t,
+    /// First IP covered by this entry
+    start_ip: usize,
+    /// first IP NOT covered by this entry
+    end_ip: usize,
+    /// global-pointer in effect for this entry
+    global_pointer: usize,
+    /// real type: unw_dyn_info_format_t
+    format: i32,
+    pad: i32,
+    u: unw_dyn_info_union
+}
 
+pub union unw_dyn_info_union {
+    pi: ManuallyDrop<unw_dyn_proc_info_t>,
+    ti: ManuallyDrop<unw_dyn_table_info_t>,
+    rti: ManuallyDrop<unw_dyn_remote_table_info_t>,
+}
+
+#[repr(C)]
+pub struct unw_dyn_proc_info_t {
+    name_ptr: *mut CStr,
+    handler: *mut (),
+    flags: u32,
+    pad0: i32,
+    regions: *mut ()
+}
+#[repr(C)]
+pub struct unw_dyn_table_info_t {
+    /// addr. of table name (e.g., library name)
+    name_pointer: *mut CStr,
+    /// segment base
+    segbase: usize,
+    /// must be a multiple of sizeos(unw_word_t)
+    table_len: usize,
+    table_data: *mut usize
+}
+#[repr(C)]
+pub struct unw_dyn_remote_table_info_t {
+    /// addr. of table name (e.g., library name)
+    name_ptr: *mut CStr,
+    /// segment base
+    segbase: usize,
+    /// must be a multiple of sizeof(unw_word_t)!
+    table_len: usize,
+    table_data: usize,
+}
 
 // functions
 extern "C" {
